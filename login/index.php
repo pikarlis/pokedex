184aa840b87286673e6eccf6b0c3a0db507b5dd9
pikarlis <kajas2013@seznam.cz>
2018-06-16 22:22:16 +0200

    first time setup

---
+++ login/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="main-wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                </ul>
+                <div class="nav-login">
+                    <form>
+                        <input type="text" name="uid" placeholder="Username / E-mail">
+                        <input type="password" name="pwd" placeholder="password">
+                        <button type="submit" name="submit">Login</button>
+                    </form>
+                    <a href="singup.php">Sing Up</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <section>
+
+    </section>
+</body>
+</html>
